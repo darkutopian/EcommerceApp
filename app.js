5f239ff822e2aa7a94f4be080d2fb1d570a839b0
darkutopian <hadikhancr7@gmail.com>
2021-01-01 20:09:19 +0530

    An e commerce app

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://hadi:7dark7robot7@cluster0.qrre8.mongodb.net/<dbname>?retryWrites=true&w=majority'
+);
+
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === 'Options') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
